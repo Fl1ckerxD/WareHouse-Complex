8366ecc0ed452a2dbe238be8d95792376f27dca7
Fl1ckerxD <mihaylav.slava@yandex.ru>
2021-12-03 00:24:15 +0300

    Test commit 2

--- Warehouse Complex/Directory.cs
+++ Warehouse Complex/Directory.cs
@@ -62,5 +62,9 @@ namespace Warehouse_Complex
         {
             mySqlAplication.SearchItem(dataGridView_WarehouseWorkers, cb_columnsWarehouseWorkers, tb_searcherWarehouseWorkers);
         }
+        public void sf()
+        {
+            string sf = "fasf";
+        }
     }
 }

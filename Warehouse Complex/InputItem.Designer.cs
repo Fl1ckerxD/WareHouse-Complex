eef33c0cea94c9a46972b223c07cc15dee5fe000
Fl1ckerxD <mihaylav.slava@yandex.ru>
2021-12-03 23:16:44 +0300

    Add elements on InputItem

--- Warehouse Complex/InputItem.Designer.cs
+++ Warehouse Complex/InputItem.Designer.cs
@@ -30,6 +30,12 @@ namespace Warehouse_Complex
         private void InitializeComponent()
         {
             this.dataGridViewInputItem = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.cb_goodId = new System.Windows.Forms.ComboBox();
+            this.cb_nameSupplier = new System.Windows.Forms.ComboBox();
+            this.cb_fio = new System.Windows.Forms.ComboBox();
+            this.tb_fioGiver = new System.Windows.Forms.TextBox();
+            this.b_add = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInputItem)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,11 +49,65 @@ namespace Warehouse_Complex
             this.dataGridViewInputItem.Size = new System.Drawing.Size(800, 252);
             this.dataGridViewInputItem.TabIndex = 0;
             // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(588, 12);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 1;
+            // 
+            // cb_goodId
+            // 
+            this.cb_goodId.FormattingEnabled = true;
+            this.cb_goodId.Location = new System.Drawing.Point(217, 47);
+            this.cb_goodId.Name = "cb_goodId";
+            this.cb_goodId.Size = new System.Drawing.Size(121, 21);
+            this.cb_goodId.TabIndex = 2;
+            // 
+            // cb_nameSupplier
+            // 
+            this.cb_nameSupplier.FormattingEnabled = true;
+            this.cb_nameSupplier.Location = new System.Drawing.Point(445, 47);
+            this.cb_nameSupplier.Name = "cb_nameSupplier";
+            this.cb_nameSupplier.Size = new System.Drawing.Size(121, 21);
+            this.cb_nameSupplier.TabIndex = 3;
+            // 
+            // cb_fio
+            // 
+            this.cb_fio.FormattingEnabled = true;
+            this.cb_fio.Location = new System.Drawing.Point(217, 115);
+            this.cb_fio.Name = "cb_fio";
+            this.cb_fio.Size = new System.Drawing.Size(121, 21);
+            this.cb_fio.TabIndex = 4;
+            // 
+            // tb_fioGiver
+            // 
+            this.tb_fioGiver.Location = new System.Drawing.Point(445, 116);
+            this.tb_fioGiver.Name = "tb_fioGiver";
+            this.tb_fioGiver.Size = new System.Drawing.Size(121, 20);
+            this.tb_fioGiver.TabIndex = 5;
+            // 
+            // b_add
+            // 
+            this.b_add.Location = new System.Drawing.Point(355, 160);
+            this.b_add.Name = "b_add";
+            this.b_add.Size = new System.Drawing.Size(75, 23);
+            this.b_add.TabIndex = 6;
+            this.b_add.Text = "Добавить";
+            this.b_add.UseVisualStyleBackColor = true;
+            this.b_add.Click += new System.EventHandler(this.b_add_Click);
+            // 
             // InputItem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.b_add);
+            this.Controls.Add(this.tb_fioGiver);
+            this.Controls.Add(this.cb_fio);
+            this.Controls.Add(this.cb_nameSupplier);
+            this.Controls.Add(this.cb_goodId);
+            this.Controls.Add(this.dateTimePicker);
             this.Controls.Add(this.dataGridViewInputItem);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "InputItem";
@@ -55,11 +115,18 @@ namespace Warehouse_Complex
             this.Load += new System.EventHandler(this.InputItem_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInputItem)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridViewInputItem;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.ComboBox cb_goodId;
+        private System.Windows.Forms.ComboBox cb_nameSupplier;
+        private System.Windows.Forms.ComboBox cb_fio;
+        private System.Windows.Forms.TextBox tb_fioGiver;
+        private System.Windows.Forms.Button b_add;
     }
 }
--- Warehouse Complex/InputItem.cs
+++ Warehouse Complex/InputItem.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Warehouse_Complex
 {
@@ -26,6 +27,19 @@ namespace Warehouse_Complex
                 "dbo.Товары ON dbo.[Поставки].Товар_Id = dbo.Товары.Id "+
                 "FULL OUTER JOIN dbo.Поставщики ON dbo.Поставщики.Id = dbo.Поставки.[Наименование поставщика] "+
                 "FULL OUTER JOIN dbo.[Работники склада] on dbo.Поставки.[ФИО принявшего товар] = dbo.[Работники склада].Id", dataGridViewInputItem);
+        }
+
+        private void b_add_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand(
+                "INSERT INTO [Поставки] (Дата, Товар_Id, [Наименование поставщика], [ФИО принявшего товар], [ФИО сдавшего товар]) " +
+                "VALUES (@Дата, @Товар_Id, @[Наименование поставщика], @[ФИО принявшего товар], @[ФИО сдавшего товар])",mySqlAplication.sqlConnection);
+
+            command.Parameters.AddWithValue("Дата", dateTimePicker.Value);
+            command.Parameters.AddWithValue("Товар_Id", cb_goodId.Text);
+            command.Parameters.AddWithValue("[Наименование поставщика]", cb_nameSupplier.Text);
+            command.Parameters.AddWithValue("[ФИО принявшего товар]", cb_fio.Text);
+            command.Parameters.AddWithValue("[ФИО сдавшего товар]", tb_fioGiver.Text);
         }
     }
 }

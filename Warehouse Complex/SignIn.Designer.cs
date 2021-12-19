aa9421412e9e2347165d48222807ddf3fb2b0dc2
Fl1ckerxD <mihaylav.slava@yandex.ru>
2021-12-19 03:11:28 +0300

    Add authorization

---
+++ Warehouse Complex/SignIn.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Warehouse_Complex
+{
+    partial class SignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_login = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.b_signin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_login
+            // 
+            this.tb_login.Location = new System.Drawing.Point(315, 105);
+            this.tb_login.Name = "tb_login";
+            this.tb_login.Size = new System.Drawing.Size(100, 20);
+            this.tb_login.TabIndex = 0;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(315, 162);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(100, 20);
+            this.tb_password.TabIndex = 1;
+            // 
+            // b_signin
+            // 
+            this.b_signin.Location = new System.Drawing.Point(328, 223);
+            this.b_signin.Name = "b_signin";
+            this.b_signin.Size = new System.Drawing.Size(75, 23);
+            this.b_signin.TabIndex = 2;
+            this.b_signin.Text = "Войти";
+            this.b_signin.UseVisualStyleBackColor = true;
+            this.b_signin.Click += new System.EventHandler(this.b_signin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(315, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пароль";
+            // 
+            // SignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b_signin);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_login);
+            this.Name = "SignIn";
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_login;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Button b_signin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Warehouse Complex/SignIn.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Warehouse_Complex
+{
+    public partial class SignIn : Form
+    {
+        MySqlAplication mySqlAplication = new MySqlAplication();
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void b_signin_Click(object sender, EventArgs e)
+        {
+            if(tb_login.Text == String.Empty || tb_password.Text == String.Empty)
+            {
+                MessageBox.Show("Не все данные введены", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                mySqlAplication.Connect();
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter($"SELECT * FROM [Логины] WHERE Логин = N'{tb_login.Text}' AND Пароль = N'{tb_password.Text}'", mySqlAplication.sqlConnection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                if (dataTable.Rows.Count > 0)
+                {
+                    switch (dataTable.Rows[0][2])
+                    {
+                        case 1:
+                            ShowMainWin();
+                            break;
+
+                        case 2:
+                            ShowMainWin();
+                            break;
+
+                        case 3:
+                            ShowMainWin();
+                            break;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Не верный логин или пароль", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }          
+        }
+        private void ShowMainWin()
+        {
+            Form1 mainWin = new Form1();
+            mainWin.Show();
+            this.Hide();
+        }
+    }
+}
